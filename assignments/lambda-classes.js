d0bb3200f652b25053bb0e762c8e993f428e6837
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-09 12:04:03 +0100

    Create Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,17 @@
 // CODE here for your Lambda Classes
+
+// PERSON CLASS
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+  }
+
+  speak() {
+      console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+  }
+}
+
+// INSTRUCTOR CLASS
